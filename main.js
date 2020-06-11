06ec0801c08526f86822d20f9d4a2641ccc4ddd9
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-11 10:36:32 +0200

    Grafico LINE appunti + qualche correzione

--- main.js
+++ main.js
@@ -8,65 +8,113 @@ $(document).ready(function() {
 // un grafico a torta per rappresentare le vendite di ogni venditore
 
 
+//creo la variabile dell'url di chiamata ajax
+var url_personale = "http://157.230.17.132:4010/sales"
+
     //chiamata ajax
     $.ajax({
-        'url': "http://157.230.17.132:4010/sales",
+        'url': url_personale,
         'method': 'GET',
-        'success': function (data) {
+        'success': function (data) { // + log dei dati (data) da fare sempre quando iniziamo => vediamo cosa arriva dall'API
+
+            //creo un oggetto con i mesi: posso scriverlo a mano perchè i mesi sono 12 e non cambiano, oppure farli scrivere a moment
+            var mese = {
+                'gennaio': 0,
+                'febbraio': 0,
+                'marzo': 0,
+                'aprile': 0,
+                'maggio': 0,
+                'giugno': 0,
+                'luglio': 0,
+                'agosto': 0,
+                'settembre': 0,
+                'ottobre': 0,
+                'novembre': 0,
+                'dicembre': 0
+            };//fine oggetto mese
 
             var venditori = {};
-            var mese = {};
+
             //ciclo data per tirarmi fuori gli elementi che mi servono. In questo caso l'API mi restituisce un array di oggetti che posso ciclare per tirare fuori i singoli oggetti su cui applicare la dot.notation per ottenere gli elementi che mi servono, MA! non è sempre valido. Ogni API è diverso, prima guardare sempre cosa contiene l'API per valutare come tirare fuori i dati in base a come è costruito => non c'è una regola! Dipende da come è costruita l'API.
             for (var i = 0; i < data.length; i++) {
-                // recupero l'elemento corrente, che corrisponde ad un oggetto
-                // questo oggetto rappresenta le vendite di un determinato venditore in una determinata data
+                //mi estraggo un oggetto alla volta
                 var vendita_corrente = data[i];
                 //con la dot notation recupero dall'oggetto i dati che mi servono per costruire i grafici
 
-                //per il grafico LINE:
+                //estrapolo le informazioni:
                 //la data
-                var giorno_corrente = data[i].date;
-
-                //estrapolo il mese con moment
+                var giorno_corrente = vendita_corrente.date;
+                //importo momenti in tutte le lingue (url locales in html) per avere i mesi in italiano
+                moment.locale('it');
+                //estrapolo il mese con moment nel formato esteso
                 var mese_corrente = moment(giorno_corrente, 'DD/MM/YYYY').format('MMMM');
                 //l'importo delle vendite
-                var importo_corrente = data[i].amount;
-
-                //per il grafico PIE:
+                var importo_corrente = parseInt(vendita_corrente.amount);
                 //i venditori
-                var venditore_corrente = data[i].salesman;
+                var venditore_corrente = vendita_corrente.salesman;
 
+                //LINE
+                //prendo l'oggetto[accedo alla chiave con il suo valore] e aggiungo l'importo
+                mese[mese_corrente] += importo_corrente;
 
+                //PIE
                 //verifico se l'oggetto dei venditori contiene già la chiave del venditore corrente
                 if (!venditori.hasOwnProperty(venditore_corrente)) {
                     // il venditore corrente NON è presente nell'oggetto dei venditori
                     // creo una nuova chiave con il venditore corrente
                     // e assegno il valore con la vendita corrente
+                    //prendo l'oggetto[accedo alla chiave con il suo valore]
                     venditori[venditore_corrente] = importo_corrente;
                 } else {
                     //se è già presente, somma il valore della vendita corrispondente a quello già inserito
                     venditori[venditore_corrente] += importo_corrente;
                 }
-
-                // verifico se l'oggetto dei mesi contiene già la chiave del mese corrente
-                if (!mese.hasOwnProperty(mese_corrente)) {
-                    // il mese corrente NON è presente nell'oggetto dei mesi
-                    // creo una nuova chiave con il mese corrente
-                    // e assegno il valore con la vendita corrente
-                    mese[mese_corrente] = importo_corrente;
-                } else {
-                    //se è già presente, somma il valore della vendita corrispondente a quello già inserito
-                    mese[mese_corrente] += importo_corrente;
-                }
-
-
             };//fine ciclo for
+
+            //GRAFICO LINE VENDITE MENSILI
+            //creo una variabile con le chiavi dell'oggetto mese => corrisponde al mese in esame
+            var chiavi_line = Object.keys(mese);
+            //creo  una variabile con i valori dell'oggetto mese => corrisponde all'importo delle vendite per quel mese
+            var valori_line = Object.values(mese);
+
+
+            var ctx = $('#linee-mese')[0].getContext('2d');
+
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: chiavi_line,
+                    datasets: [{
+                        label: '€',
+                        data: valori_line,
+                        pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                        borderColor: 'rgba(0, 0, 0, 0,5)',
+                        fill: false,
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: 'Grafico delle vendite per mese:',
+                        fontSize: 25
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });//fine grafico line
 
 
             //creo una variabile con le chiavi dell'oggetto venditori => corrisponde al nome del venditore
             var chiavi = Object.keys(venditori);
             //creo  una variabile con i valori dell'oggetto venditori => corrisponde all'importo delle vendite
             var valori = Object.values(venditori);
+            console.log(valori);
 
             //primo grafico torta
             var ctx = $('#torta-venditore')[0].getContext('2d');
@@ -101,61 +149,7 @@ $(document).ready(function() {
                 }
             });
 
-            //creo una variabile con le chiavi dell'oggetto mese => corrisponde al mese in esame
-            var chiavi_line = Object.keys(mese);
-            console.log(chiavi_line);
-            //creo  una variabile con i valori dell'oggetto mese => corrisponde all'importo delle vendite per quel mese
-            var valori_line = Object.values(mese);
-            console.log(valori_line);
 
-            //Secondo grafico linea
-            var ctx = $('#linee-mese')[0].getContext('2d');
-
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: chiavi_line,
-                    datasets: [{
-                        label: '€',
-                        data: valori_line,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(200, 99, 132, 0.2)',
-                            'rgba(80, 20, 235, 0.2)',
-                            'rgba(300, 50, 86, 0.2)',
-                            'rgba(255, 100, 100, 0.2)',
-                            'rgba(0, 102, 255, 0.2)',
-                            'rgba(90, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(200, 99, 132, 1)',
-                            'rgba(80, 20, 235, 1)',
-                            'rgba(300, 50, 86, 1)',
-                            'rgba(255, 100, 100, 1)',
-                            'rgba(0, 102, 255, 1)',
-                            'rgba(90, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    title: {
-                        display: true,
-                        text: 'Grafico delle vendite per mese:'
-                    }
-                }
-            });
 
         },// fine success
         'error': function () {
